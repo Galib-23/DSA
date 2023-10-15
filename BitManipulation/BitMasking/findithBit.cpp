3af512c6ed1255f6f6cdfc80b9231605041d3173
Galib-23 <aagalib2323@gmail.com>
2023-10-15 22:53:14 +0600

    findithBit added

--- BitManipulation/BitMasking/findithBit.cpp
+++ BitManipulation/BitMasking/findithBit.cpp
@@ -8,9 +8,28 @@ mask--> 0 0 0 1 0 0 0 0 0 (This is made by 1 << i where i = 5 for this case)
 (&)oper:0 0 0 1 0 0 0 0 0
 
 */
+//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
 #include<bits/stdc++.h>
 using namespace std;
-int main () {
-    
+
+bool isBitSet(int num, int i){
+    int mask = 1 << i;
+    if((num & mask) != 0){
+        return true;
+    }
+    else return false;
 }
-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
+
+int main () {
+    int num, i;
+    cout<<"Enter the number: ";
+    cin>>num;
+    cout<<"Enter the position: ";
+    cin>>i;
+    if(isBitSet(num, i)){
+        cout<<"The bit is 1"<<endl;
+    }
+    else{
+        cout<<"The bit is 0"<<endl;
+    }
+}
