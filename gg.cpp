d867e759aa17cf91730a5c64248d158613790d5e
Galib-23 <aagalib2323@gmail.com>
2024-01-04 21:18:11 +0600

    Algo

--- gg.cpp
+++ gg.cpp
@@ -1,56 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int toMultiply(int x)
-{
-    int y = 1;
-    for (int i = 1; i < x; i++)
-    {
-        y = y * 10;
-    }
-    return y;
-}
-
 int main()
 {
-    int t;
-    cin >> t;
-    while (t--)
+    int n, k;
+    cin>>n>>k;
+    int a[n];
+    for (int i = 0; i < n; i++)
     {
-        int n;
-        cin >> n;
-        stack<int> st;
-        while (n)
-        {
-            int rem = n % 10;
-            st.push(rem);
-            n = n / 10;
-        }
-        vector<int> v;
-        while (!st.empty())
-        {
-            int tmp = st.top();
-            if (st.size() == 1)
-            {
-                if (tmp != 0)
-                    v.push_back(tmp);
-            }
-            else
-            {
-                int toM = toMultiply(st.size());
-                if (tmp * toM != 0)
-                    v.push_back(tmp * toM);
-            }
-            st.pop();
-        }
-
-        cout << v.size() << endl;
-        for (int i = 0; i < v.size(); i++)
-        {
-            // if(v[i] != 0){
-            cout << v[i] << " ";
-            //  }
-        }
-        cout << endl;
+        cin>>a[i];
     }
+    sort(a, a + n);
+    int count = 0;
+    int team = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if(a[i] + k <= 5){
+            count++;
+        }else break;
+        if(count == 3){
+            team++;
+            count = 0;
+        }
+    }
+    cout<<team<<endl;
+    
 }
