e979c90f407e421680d4b4ba430449deada5f752
Galib-23 <aagalib2323@gmail.com>
2026-03-09 02:15:04 +0600

    bfs

--- Tree/3-bfsTree.cpp
+++ Tree/3-bfsTree.cpp
@@ -30,17 +30,28 @@ Node* createTreeFromPreorderArray(vector<int>& preorder, int& i) {
   return newNode;
 }
 
-void printTree(Node* root) {
-  if(root == nullptr) return;
-  printTree(root->left);
-  cout<<root->val<< " ";
-  printTree(root->right);
-  return;
+void bfs(Node* root, queue<Node*>& q) {
+  while(!q.empty()) {
+    cout<<q.front()->val<<" ";
+    if(q.front()->left != nullptr) q.push(q.front()->left);
+    if(q.front()->right != nullptr) q.push(q.front()->right);
+    q.pop();
+    if(q.front() == nullptr and q.size() == 1) {
+      break;
+    } else if(q.front() == nullptr and q.size() > 1) {
+      q.pop();
+      q.push(nullptr);
+      cout<<endl;
+    }
+  }
 }
 
 int main() {
   vector<int> preorder = {2, 4, 8, -1, -1, 10, -1, -1, 6, 7, -1, -1};
   int i = 0;
   Node* root = createTreeFromPreorderArray(preorder, i);
-  printTree(root);
+  queue<Node*> q;
+  q.push(root);
+  q.push(nullptr);
+  bfs(root, q);
 }
